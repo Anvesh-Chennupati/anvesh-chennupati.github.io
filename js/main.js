4a2dc9f07cd4f398b1668bf6ba93469b174480b4
Anvesh Chennupati <anveshchowdary01@gmail.com>
2018-08-03 15:16:09 +0530

    particles animation

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ $(window).on('load', function () { // makes sure that whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(400).fadeOut('slow');
 });
+
+// particles
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '../js/particles/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
