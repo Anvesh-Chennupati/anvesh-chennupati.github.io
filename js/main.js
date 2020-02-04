fbe0140ea8ab5e815d09637241ca6c3bbb902bf8
Anvesh <anveshchowdary01@gmail.com>
2020-02-04 00:00:09 -0800

    fixed navlist click

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ $(document).ready(function () {
         $('.top-nav').toggleClass('open');
     })
 });
+
+$('.top-nav .nav-link').on('click', function() {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+});
