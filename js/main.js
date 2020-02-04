6c3864b8e1550888a183e77753a355c5ac6debb8
Anvesh <anveshchowdary01@gmail.com>
2020-02-04 00:22:46 -0800

    added js up and scroll

--- js/main.js
+++ js/main.js
@@ -8,9 +8,25 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     })
+
+    // for colapsing the menu toggler
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    
+    // for scrollling 
+    $('nav a[href*="#"]').on('click', function (){
+        $('html ,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, 2000);
+    })
+
+    // up button works
+    $('#up').on('click', function (){
+        $('html ,body').animate({
+            scrollTop: 0
+        }, 2000);
+    })
 });
 
-$('.top-nav .nav-link').on('click', function() {
-    $('.menu-toggler').removeClass('open');
-    $('.top-nav').removeClass('open');
-});
